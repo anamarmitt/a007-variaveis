ded9a6658640cc22a53813c22a4ee6864518d37a
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-08-29 13:31:04 -0300

    ex2-feito

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let q1 = "Você tomou café hoje?";
+let response1 = prompt(q1);
+
+let q2 = "Você toma café todos os dias?";
+let response2 = prompt(q2);
+
+let q3 = "Você é viciado em café?";
+let response3 = prompt(q3);
+
+console.log(q1, response1);
+console.log(q2, response2);
+console.log(q3, response3);
