e095993148c4547b3b2e382fa692556287fa1b89
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-08-29 16:18:33 -0300

    ex2-nomesMelhorados

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,12 +1,12 @@
-let q1 = "Você tomou café hoje?";
-let response1 = prompt(q1);
+let question1 = "Você tomou café hoje?";
+let response1 = prompt(question1);
 
-let q2 = "Você toma café todos os dias?";
-let response2 = prompt(q2);
+let question2 = "Você toma café todos os dias?";
+let response2 = prompt(question2);
 
-let q3 = "Você é viciado em café?";
-let response3 = prompt(q3);
+let question3 = "Você é viciado em café?";
+let response3 = prompt(question3);
 
-console.log(q1, response1);
-console.log(q2, response2);
-console.log(q3, response3);
+console.log(question1, response1);
+console.log(question2, response2);
+console.log(question3, response3);
